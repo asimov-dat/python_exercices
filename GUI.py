8a7d1c32db3347e0b200c3252df5f080b16b435e
asimov-dat <datdie@outlook.com>
2022-01-18 02:03:01 +0100

    added exercices from mac

---
+++ GUI.py
@@ -0,0 +1,18 @@
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+i=0
+for row in picture:
+    while i < len(picture):
+        print(row)
+        break
+    i+=1
+
+print(len(picture))
