8c30043ab784bda925c8cd98512f648f8678c25a
asimov-dat <david.alvareztam@upb.edu.co>
2022-02-01 10:07:16 +0100

    Fix scrip

--- file_IO/script.py
+++ file_IO/script.py
@@ -1,7 +1,7 @@
 import clipboard as c
 try:
     with open('/home/dat/code/token.txt',mode = 'r') as my_file:
-        token = my_file.read()
+        token = my_file.readline()
         c.copy(token)
 except FileNotFoundError as err:
     print('file does not exist')
